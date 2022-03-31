311bc841f41a4e649cc6f70c40cdf5ec7364551f
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2022-03-30 20:48:22 -0400

    Added the methods needed for the partner invite controller

---
+++ app/Http/Controllers/InvitePartnerController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PartnerInvite;
+use App\User;
+use Illuminate\Http\Request;
+
+class InvitePartnerController extends Controller
+{
+    //show a user a form with an email field to invite a new user
+    public function invite(){
+        return view('partner.invite');
+    }
+
+    //process the form submission and send the invite by email
+    public function process(Request $request){
+        //Validating the incoming request data
+        do{
+            //Generate a random token
+            $token = str_random();
+
+        }//keep generating a new token if the token already exists
+        while(PartnerInvite::where('token', $token)->first());
+
+        //Create a new invite record and store it in the partner_invites table
+        $invite = PartnerInvite::create([
+            'email' => $request->get('email'),
+            'token' => $token,
+        ]);
+
+        //Send the email using the mail class that we created
+
+        //redirect the user to where they came from
+        return redirect()->back();
+
+    }
+
+    //here we'll look up the user by the token sent provided in the URL
+    public function accept($token){
+        //Handling if the invite doesn't exist
+        if(!$invite = PartnerInvite::where('token', $token)->first()){
+            //Abort the task
+            abort(404);
+        }
+
+        //Created the user with the details from the invite
+        User::where('email', $invite->email)->first();
+
+        //Delete the invite record so it is not reused
+        $invite->delete();
+
+        //Here we will put the actions that will happen after the invitation is done successfully but for now we will prove it worked
+        return 'Good job! Invite accepted!';
+    }
+}
---
+++ resources/views/partner/invite.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            display: flex;
+            flex-wrap: nowrap;
+            overflow: hidden;
+        }
+
+        body > div {
+            width: 100px;
+            margin: 10px;
+        }
+
+        .main {
+            flex-basis: 80%;
+        }
+
+        .side-nav {
+            flex-basis: 20%;
+        }
+    </style>
+</head>
+<body>
+<div class="side-nav">
+    @include('partials.navigation')
+</div>
+// make use of the named route so that if the URL ever changes,
+// the form will not break #winning
+<form action="{{ route('invite') }}" method="post">
+    {{ csrf_field() }}
+    <input type="email" name="email"/>
+    <button type="submit">Send invite</button>
+</form>
+</body>
+</html>
