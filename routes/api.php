2019e7ea9c59a8175ccde32c0e28dd3ca494a6c7
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2022-04-03 15:02:46 -0400

    Finished setting up the routes, installing passport and other changes needed for the app authentication

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['prefix' => 'v1'], function () {
+    Route::post('/login', 'UsersController@login');
+    Route::post('/register', 'UsersController@register');
+    Route::get('/logout', 'UsersController@logout')->middleware('auth:api');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
