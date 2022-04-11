3542641a6fa290bc04f19b055cc254fbed950660
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2022-04-11 19:06:44 -0400

    Themed the edit task page

--- resources/views/planning/edit.blade.php
+++ resources/views/planning/edit.blade.php
@@ -10,8 +10,10 @@
     <style>
         body {
             font-family: 'Roboto Slab';
-            width: 900px;
-            margin: 0 auto;
+        }
+
+        .main{
+            margin: 45px 5% 5%;
         }
 
         h1 {
@@ -20,70 +22,79 @@
             text-align: center;
         }
 
-        label{
+        label {
             color: #61d2c6;
         }
 
-        .form-control{
+        .form-control {
             height: 45px;
             /*box-shadow: 0 1rem 2rem 0 rgba(0, 0, 0, 0.1);*/
         }
 
         button{
+            color: white;
             height: 45px;
             width: 100%;
-            color: white;
             background-color: #ff8585;
         }
     </style>
 </head>
 <body>
-<h1>Edit Task</h1>
-<form method="POST" action="{{ action('TaskController@update', $task->id) }}">
-    {{method_field('PATCH')}}
-    {{ csrf_field() }}
-    <div class="form-group">
-        <label for="exampleInputEmail1">Task</label>
-        <input name="task_title" type="text" class="form-control" id="task_title"
-               placeholder="Task Title" value="{{$task->task_title}}">
-        {{--        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>--}}
-    </div>
-    <div class="form-group">
-        <label for="task_description">Description</label>
-        <input name="task_description" type="text" class="description form-control" id="task_description"
-               placeholder="Task Description" value="{{$task->task_description}}">
-    </div>
-    <div class="form-group">
-        <label for="due_date">Due date</label>
-        <input name="due_date" type="text" class="form-control" id="due_date"
-               placeholder="YYYY-MM-DD" value="{{$task->due_date}}">
-    </div>
-    <div class="form-group">
-        <label for="assigned_user">Assigned to</label>
-        <input name="assigned_user" type="text" class="form-control" id="assigned_user"
-               placeholder="Choose One" value="{{$task->assigned_user}}">
-    </div>
-    <div class="form-group">
-        <label for="task_price">Cost</label>
-        <input name="task_price" type="text" class="form-control" id="task_price"
-               placeholder="e.g 22.00, 25, 76.00, etc" value="{{$task->task_price}}">
-    </div>
-    <div class="form-group">
-        <div class="dropdown">
-            <label for="budget_category_id">Category</label>
-            <select class="form-control" name="budget_category_id">
-                @foreach ($categories as $id => $category)
-                    <option value="{{$id}}">{{$category}}</option>
-                @endforeach
-            </select>
+<div class="row">
+    <div class="col-sm-3">
+        <div class="item side-nav">
+            @include('partials.navigation')
         </div>
     </div>
-    <button type="submit" class="btn">Save Changes</button>
-</form>
-@if($errors->any())
-    @foreach($errors->all() as $error)
-        {{$error}}<br>
-    @endforeach
-@endif
+    <div class="col-sm-6">
+        <div class="main">
+            <h1>Edit Task</h1>
+            <form method="POST" action="{{ action('TaskController@update', $task->id) }}">
+                {{method_field('PATCH')}}
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Task</label>
+                    <input name="task_title" type="text" class="form-control" id="task_title"
+                           placeholder="Task Title" value="{{$task->task_title}}">
+                    {{--        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>--}}
+                </div>
+                <div class="form-group">
+                    <label for="task_description">Description</label>
+                    <input name="task_description" type="text" class="description form-control" id="task_description"
+                           placeholder="Task Description" value="{{$task->task_description}}">
+                </div>
+                <div class="form-group">
+                    <label for="due_date">Due date</label>
+                    <input name="due_date" type="text" class="form-control" id="due_date"
+                           placeholder="YYYY-MM-DD" value="{{$task->due_date}}">
+                </div>
+                <div class="form-group">
+                    <label for="assigned_user">Assigned to</label>
+                    <input name="assigned_user" type="text" class="form-control" id="assigned_user"
+                           placeholder="Choose One" value="{{$task->assigned_user}}">
+                </div>
+                <div class="form-group">
+                    <label for="task_price">Cost</label>
+                    <input name="task_price" type="text" class="form-control" id="task_price"
+                           placeholder="e.g 22.00, 25, 76.00, etc" value="{{$task->task_price}}">
+                </div>
+                <div class="form-group">
+                    <div class="dropdown">
+                        <label for="budget_category_id">Category</label>
+                        <select class="form-control" name="budget_category_id">
+                            @foreach ($categories as $id => $category)
+                                <option value="{{$id}}">{{$category}}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <button type="submit" class="btn">Save Changes</button>
+            </form>
+            @if($errors->any())
+                @foreach($errors->all() as $error)
+                    {{$error}}<br>
+                @endforeach
+            @endif
+        </div>
 </body>
 </html>
