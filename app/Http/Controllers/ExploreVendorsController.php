1b10b5c173eb71addd714e44ec6abaffd4aa8c3d
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2022-04-22 21:49:33 -0400

    Fixed last bugs

--- app/Http/Controllers/ExploreVendorsController.php
+++ app/Http/Controllers/ExploreVendorsController.php
@@ -19,18 +19,37 @@ class ExploreVendorsController extends Controller
     //All tasks
     public function index()
     {
-        $vendorCategories = ["Venues", "Boutiques", "Photography", "Flower Stores", "Gifts"];
+        $vendorCategories = ["Venue", "Gown", "Photographer", "Florist", "Gift"];
 
         return view('explore-vendors.index', compact("vendorCategories"));
     }
 
     //A task details
-    public function show()
+    public function show($vendor)
     {
+        switch ($vendor){
+            case "Venue":
+                $url = 'https://api.foursquare.com/v3/places/search?query=Club&fields=name%2Clocation%2Ctel%2Cwebsite%2Crating%2Cphotos&near=Windsor%2C%20ON';
+                break;
+            case "Gown":
+                $url = 'https://api.foursquare.com/v3/places/search?query=Boutique&fields=name%2Clocation%2Cdescription%2Cwebsite%2Crating%2Cphotos&near=Windsor%2C%20ON';
+                break;
+            case "Photographer":
+                $url = 'https://api.foursquare.com/v3/places/search?query=Photo&fields=name%2Clocation%2Cdescription%2Cwebsite%2Crating%2Cphotos&near=Windsor%2C%20ON';
+                break;
+            case "Florist":
+                $url = 'https://api.foursquare.com/v3/places/search?query=Florist&fields=name%2Clocation%2Cdescription%2Cwebsite%2Crating%2Cphotos&near=Windsor%2C%20ON';
+                break;
+            case "Gift":
+                $url = 'https://api.foursquare.com/v3/places/search?query=Wedding%20Gift&fields=name%2Clocation%2Cdescription%2Cwebsite%2Crating%2Cphotos&near=Windsor%2C%20ON';
+                break;
+            default:
+                $url = 'https://api.foursquare.com/v3/places/search?query=Boutique&fields=name%2Clocation%2Cdescription%2Cwebsite%2Crating%2Cphotos&near=Windsor%2C%20ON';
+        }
 
         $client = new GuzzleHttp\Client();
 
-        $response = $client->request('GET', 'https://api.foursquare.com/v3/places/search?query=Clubs&fields=name%2Clocation%2Cdescription%2Cwebsite%2Crating%2Cphotos&near=Windsor%2C%20ON', [
+        $response = $client->request('GET', $url, [
             'headers' => [
                 'Accept' => 'application/json',
                 'Authorization' => 'fsq3+0NhCeZmuY7A4sPIaenwvm5mBuVG8SA63KcT9o9ZEsE=',
@@ -52,6 +71,7 @@ class ExploreVendorsController extends Controller
     }
 
     public function showDetails($vendorName){
+
         $client = new GuzzleHttp\Client();
 
         $response = $client->request('GET', 'https://api.foursquare.com/v3/places/search?query=Clubs&fields=name%2Clocation%2Cdescription%2Cwebsite%2Crating%2Cphotos&near=Windsor%2C%20ON', [
@@ -75,7 +95,6 @@ class ExploreVendorsController extends Controller
                 $vendor = $item;
             }
         }
-
         return view('explore-vendors.details', compact("vendor"));
     }
 }
